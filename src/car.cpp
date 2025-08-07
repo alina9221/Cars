27f372c1c985cbc64764553f3025c86e598f5504
alina9221 <alina9221@yandex.ru>
2025-08-07 22:39:00 +0300

    car.cpp

--- src/car.cpp
+++ src/car.cpp
@@ -1,32 +1,37 @@
-#include <src/car.h>
 #include "car.h"
 
-void Car::setName(const string &name)
-{
-    name_ = name;
+int Car::count = 0;
+
+Car::Car() : trade_mark(""), cylinders(0), power(0) {
+    count++;
 }
 
-const string &Car::getName() const
-{
-    return name_;
+Car::Car(const std::string& tm, int cyl, int pwr) 
+    : trade_mark(tm), cylinders(cyl), power(pwr) {
+    count++;
 }
 
-void Car::setCylinder(int cylinder)
-{
-    cylinder_ = cylinder;
+Car::~Car() {
+    count--;
 }
 
-int Car::getCylinder() const
-{
-    return cylinder_;
+std::ostream& operator<<(std::ostream& os, const Car& car) {
+    os << "Marka: " << car.trade_mark 
+       << ", Cylinder: " << car.cylinders 
+       << ", moshnost: " << car.power;
+    return os;
 }
 
-void Car::setPower(int power)
-{
-    power_ = power;
+std::istream& operator>>(std::istream& is, Car& car) {
+    std::cout << "Vvedite Marku: ";
+    is >> car.trade_mark;
+    std::cout << "Vvedite kol-vo cylindrov: ";
+    is >> car.cylinders;
+    std::cout << "Vvedite moshnost: ";
+    is >> car.power;
+    return is;
 }
 
-int Car::getPower() const
-{
-    return power_;
+int Car::getCount() {
+    return count;
 }
