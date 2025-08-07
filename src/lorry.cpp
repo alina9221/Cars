20284f177e347aeb9dd2cd223f37247bdf28ee8b
alina9221 <alina9221@yandex.ru>
2025-08-07 22:40:29 +0300

    lorry.cpp

--- src/lorry.cpp
+++ src/lorry.cpp
@@ -1,11 +1,19 @@
-#include <src/lorry.h>
+#include "lorry.h"
 
-void Lorry::setCarrying(int carrying)
-{
-    carrying_ = carrying;
+Lorry::Lorry() : Car(), carrying(0) {}
+
+Lorry::Lorry(const std::string& tm, int cyl, int pwr, int carry)
+    : Car(tm, cyl, pwr), carrying(carry) {}
+
+std::ostream& operator<<(std::ostream& os, const Lorry& lorry) {
+    os << static_cast<const Car&>(lorry)
+       << ", Gruzopodemnost: " << lorry.carrying;
+    return os;
 }
 
-int Lorry::getCarrying() const
-{
-    return carrying_;
+std::istream& operator>>(std::istream& is, Lorry& lorry) {
+    is >> static_cast<Car&>(lorry);
+    std::cout << "Vvedite Gruzopodemnost: ";
+    is >> lorry.carrying;
+    return is;
 }
