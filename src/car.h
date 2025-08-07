906bcc0535dce0b311177e1c958f646169cd27f1
alina9221 <alina9221@yandex.ru>
2025-08-07 22:40:04 +0300

    car.h

--- src/car.h
+++ src/car.h
@@ -1,86 +1,21 @@
 #pragma once
 #include <string>
+#include <iostream>
 
-using namespace std;
-
-/// @brief Базовый класс
-class Car
-{
-private:
-    /// @brief счетчик объектов класса
-    static int count_;
-
+class Car {
 protected:
-    /// @brief марка
-    string name_;
-
-    /// @brief число цилиндров
-    int cylinder_;
-
-    /// @brief мощность
-    int power_;
+    std::string trade_mark;
+    int cylinders;
+    int power;
+    static int count; // для подсчёта объектов
 
 public:
-    /// @brief конструктор по умолчанию (без параметров)
-    Car()
-        : name_("oka"), cylinder_(2), power_(33)
-    {
-        // делаем инкремент счетчика объектов при создании нового объекта
-        ++count_;
-    }
+    Car();
+    Car(const std::string& tm, int cyl, int pwr);
+    virtual ~Car();
 
-    /// @brief конструктор с параметрами
-    /// @param theName
-    /// @param cylinders
-    /// @param power
-    Car(const string &theName, int cylinders, int power)
-        : name_(theName), cylinder_(cylinders), power_(power)
-    {
-        // делаем инкремент счетчика объектов при создании нового объекта
-        ++count_;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Car& car);
+    friend std::istream& operator>>(std::istream& is, Car& car);
 
-    /// @briefконструктор копирования
-    /// @param new_car
-    Car(const Car &new_car)
-        : name_(new_car.name_), cylinder_(new_car.cylinder_), power_(new_car.power_)
-    {
-        // делаем инкремент счетчика объектов при создании нового объекта
-        ++count_;
-    }
-
-    /// @brief получить количество созданных объектов
-    /// @return
-    friend int count(const Car& car);
-
-    /// @brief назначить марку
-    /// @param name
-    void setName(const string &name);
-
-    /// @brief получить марку
-    /// @return
-    const string &getName() const;
-
-    /// @brief назначить мощность
-    /// @param theCylinder
-    void setCylinder(int cylinder);
-
-    /// @brief получить значение мощности
-    /// @return
-    int getCylinder() const;
-
-    /// @brief назначить мощность
-    /// @param power
-    void setPower(int power);
-
-    /// @brief получить значение мощности
-    /// @return
-    int getPower() const;
-
-    /// @brief деструктор
-    ~Car()
-    {
-    }
+    static int getCount();
 };
-
-inline int Car::count_ = 0;
