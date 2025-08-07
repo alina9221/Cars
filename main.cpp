0cbc13f296f8f5ba2ead2da8001f61378aa7fd16
alina9221 <alina9221@yandex.ru>
2025-08-07 22:38:26 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,17 @@
 #include <iostream>
-#include <src/car.h>
-#include <src/lorry.h>
-#include <string>
+#include "src/car.h"
+#include "src/lorry.h"
 
-using namespace std;
+int main() {
+    Car car1;
+    std::cin >> car1;
+    std::cout << car1 << std::endl;
 
-class Small {
-private:
-    string name;
-    int cilynder;
-    int power;
+    Lorry lorry1;
+    std::cin >> lorry1;
+    std::cout << lorry1 << std::endl;
 
-public:
-    // Конструктор
-    Small(string n = "", int c = 0, int p = 0) : name(n), cilynder(c), power(p) {}
-    
+    std::cout << "Vsego obectov: " << Car::getCount() << std::endl;
 
-    // Перегрузка оператора вывода (<<)
-    friend ostream& operator<<(ostream& os, const Small& small) {
-        os << "Название: " << small.name << ", Кол-во цилиндров: " << small.cilynder << ", Мощность: " << small.power;
-        return os;
-    }
-
-    // Перегрузка оператора ввода (>>)
-    friend istream& operator >> (istream& is, Small& small) {
-        std:: cout << "Vvedite nazvanue: " << endl ;
-        std:: getline(is, small.name);  // Читаем всю строку (включая пробелы)
-        std:: cout << "Vevdite kol-o cilindrov: " << endl ;
-        is >> small.cilynder;
-        std::  cout << "Vvedite moshnost: " << endl;
-        is >> small.power;
-        is.ignore();  // Очищаем буфер после ввода числа
-        return is;
-    }
-};
-
-
-int count(const Car &car)
-{
-    return car.count_;
-}
-
-
-
-
-int main()
-{
-    //Вариант перегрузки
-    Small car_s;
-    // Вводим данные с клавиатуры
-    std:: cin >> car_s;
-    // Выводим данные
-    std:: cout << car_s << endl;
-
-
-    
-    //
-    Car mazik("Mazik", 6, 180);
-    std:: cout <<  "Car: " << mazik.getName() << " cylinders " << mazik.getCylinder() << " has power " << mazik.getPower() << std::endl; 
-    Lorry belazik("BelAZik",18,3500,180);
-    std::cout << "lorry: " << belazik.getName() << " has power " << belazik.getPower() << " cylinders " << belazik.getCylinder() << " and can carry "
-              << belazik.getCarrying() <<  std::endl;
-    std::cout << count(belazik) << std::endl;
     return 0;
 }
-
-
